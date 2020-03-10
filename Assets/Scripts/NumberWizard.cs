d6d59058ebe1504bf04672dc748eb307a5e8b468
Renato <renato.oliveira@dextra-sw.com>
2020-03-10 13:16:53 -0300

    projeto

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] bool useRandom;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame() {
+        updateGuess();
+    }
+
+    public void onPressHigher() {
+        min = Math.Min(guess+1, max);
+        updateGuess();
+    }
+
+    public void onPressLower() {
+        max = Math.Max(guess-1, min);
+        updateGuess();
+    }
+
+    private void updateGuess() {
+        if (useRandom) {
+            guess = UnityEngine.Random.Range(min, max + 1);
+        } else {
+            guess = (max + min)/2;
+        }
+        displayGuess();
+    }
+
+    private void displayGuess()
+    {
+        var guessDisplay = GameObject.Find("Guess Background/Guess Text");
+        guessDisplay.GetComponent<TextMeshProUGUI>().text = guess.ToString();
+        return;
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene(){
+        int currenSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene((currenSceneIndex+1)%SceneManager.sceneCountInBuildSettings);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
